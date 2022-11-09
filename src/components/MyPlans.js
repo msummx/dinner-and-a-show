c0b98a9e4c21939116baecaca4140af96cc92d7b
Matt Summers <mtsummersx@gmail.com>
2022-11-09 11:47:19 -0500

    added screen for myplans if no plans exist

--- src/components/MyPlans.js
+++ src/components/MyPlans.js
@@ -2,15 +2,27 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid';
 import Plan from './Plan';
+import {Link} from 'react-router-dom';
 
 const MyPlans = () => {
   const plans = useSelector(state => state.userPlans)
 
-  return (
-    <div className="events">{plans.map(plan =>{
-      return <Plan key={uuidv4()} planObj={plan}/>
-    })}</div>
-  )
+  if (plans.length > 0){
+    return (
+      <div className="events">{plans.map(plan =>{
+        return <Plan key={uuidv4()} planObj={plan}/>
+      })}</div>
+    )
+  }
+
+  else {
+    return (
+      <div className="noPlans">
+      <h2>Create some plans and find them here!</h2>
+      <Link to="/createplan" className="btn btn-light">Go Now</Link>
+      </div>
+    )
+  }
 }
 
 export default MyPlans
